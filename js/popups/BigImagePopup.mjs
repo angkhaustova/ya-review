2236ce11694a69a77e34c58de99384143383d668
Angelica Khaustova <angelicakhaustova@gmail.com>
2020-03-30 17:41:38 +0300

    Fix big image popup

--- js/popups/BigImagePopup.mjs
+++ js/popups/BigImagePopup.mjs
@@ -7,7 +7,7 @@ export default class BigImagePopup extends Popup {
 
     EventAggregator.subscribe("Card.imageClick", ({ src }) => {
       this.popup.querySelector(".popup__image").setAttribute("src", src);
-      this.open;
+      this.open();
     });
   }
 }
